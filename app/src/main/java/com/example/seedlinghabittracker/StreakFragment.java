17e8717c8359ce5199edab6a46c8d867996a413c
powalll <powallwang@gmail.com>
2020-10-16 17:05:11 -0400

    fourth commit

--- app/src/main/java/com/example/seedlinghabittracker/StreakFragment.java
+++ app/src/main/java/com/example/seedlinghabittracker/StreakFragment.java
@@ -11,12 +11,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class StreakFragment extends Fragment implements AddStreakDialogue.onInputSelected{
     private RecyclerView streakRecyclerView;
@@ -29,25 +31,14 @@ public class StreakFragment extends Fragment implements AddStreakDialogue.onInpu
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_streak, container, false);
         //streak recyclerview code
-        streakItems = new ArrayList<>();
-        streakItems.add(new StreakItem(R.drawable.ic__home_24, "Title","One Day"));
         streakRecyclerView = view.findViewById(R.id.streakRecyclerView);
-        streakRecyclerView.setHasFixedSize(true);
         streakRecyclerViewLayoutManager = new LinearLayoutManager(view.getContext());
-        streakRecyclerViewAdapter = new RecyclerViewAdapter(streakItems);
-
-        streakRecyclerView.setLayoutManager(streakRecyclerViewLayoutManager);
-        streakRecyclerView.setAdapter(streakRecyclerViewAdapter);
+        setStreakRecyclerView();
         //FAB code
         addStreakButton = (FloatingActionButton) view.findViewById(R.id.addStreakButton);
-        addStreakButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialog();
-                //dialog fragment -> fragment code
-                getDialogueFragmentInfo();
-            }
-        });
+        createFABClick();
+        //Item touch helper
+        createItemTouchHelper();
 
         return view;
     }
@@ -58,6 +49,7 @@ public class StreakFragment extends Fragment implements AddStreakDialogue.onInpu
         addStreakDialogue.show(getFragmentManager(), "add streak dialogue");
         addStreakDialogue.dismiss();
     }
+
     public void getDialogueFragmentInfo()
     {
         AddStreakDialogue dialog = new AddStreakDialogue();
@@ -69,5 +61,47 @@ public class StreakFragment extends Fragment implements AddStreakDialogue.onInpu
     public void sendInput(String input, String input2) {
         streakItems.add(new StreakItem(R.drawable.ic_plus_sign_24, input, input2));
         streakRecyclerViewAdapter.notifyDataSetChanged();
+    }
+
+    public void createFABClick()
+    {
+        addStreakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+                //dialog fragment -> fragment code
+                getDialogueFragmentInfo();
+            }
+        });
+    }
+
+    public void setStreakRecyclerView()
+    {
+        streakItems = new ArrayList<>();
+        streakItems.add(new StreakItem(R.drawable.ic__home_24, "Title","One Day"));
+        streakRecyclerView.setHasFixedSize(true);
+        streakRecyclerViewAdapter = new RecyclerViewAdapter(streakItems);
+        streakRecyclerView.setLayoutManager(streakRecyclerViewLayoutManager);
+        streakRecyclerView.setAdapter(streakRecyclerViewAdapter);
+    }
+
+    public void createItemTouchHelper()
+    {
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT, 0) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(streakItems, from, to);
+                streakRecyclerViewAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            }
+        });
+        helper.attachToRecyclerView(streakRecyclerView);
     }
 }
